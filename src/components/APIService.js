218afc474eb1a28a38bed61f8b47c1271f80a087
Sean Hinds <seanhinds81@gmail.com>
2024-04-15 16:40:42 +1000

    post working

--- src/components/APIService.js
+++ src/components/APIService.js
@@ -1,4 +1,19 @@
 export default class APIService {
+    static InsertArticle(body) {
+        return fetch(`http://localhost:5000/add`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body)
+        })
+        .then(response => response.json())
+        .catch(error => console.log(error));
+    }
+}
+
+/*
+export default class APIService {
     static PostData(name, age, date, programming) {
         return fetch(`http://localhost:5000//add_todo`, {
             method: 'POST',
@@ -14,3 +29,4 @@ export default class APIService {
         .catch(error => console.log(error));
     }
 }
+*/
--- src/pages/Tute.js
+++ src/pages/Tute.js
@@ -1,20 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import APIService from '../components/APIService';
+import Form from '../components/Form';
 
-function Tute() {
-  const [currentTime, setCurrentTime] = useState(0);
+const Tute = (props) => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
 
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+    const insertArticle = () => {
+        APIService.InsertArticle({ title, body })
+            .then((response) => props.insertedArticle(response))
+            .catch(error => console.log('error', error));
+    };
 
-  return (
-    <div >
-        <p>The current time is {currentTime}.</p>
-    </div>
-  );
-}
+    //ERROR HERE
+    const insertedArticle = (article) =>{
+        const new_articles = [...articles,article]
+        setArticles(new_articles)
+      }
+      
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        insertArticle();
+        setTitle('');
+        setBody('');
+    };
+
+    return (
+        <div>
+            <h2>Add New Article</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="body">Body:</label>
+                    <textarea
+                        id="body"
+                        value={body}
+                        onChange={(e) => setBody(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Submit</button>
+                <Form insertedArticle={insertedArticle} />
+            </form>
+        </div>
+    );
+};
 
 export default Tute;
